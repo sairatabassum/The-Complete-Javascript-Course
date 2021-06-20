f9cc7e00d340c91b97a25401d31c602337711ac7
Tabassum <sairatabassum386@gmail.com>
2021-06-20 19:43:44 +0600

    commit

---
+++ 02-Fundamental-Part-2/script.js
@@ -0,0 +1,50 @@
+
+//Activating Strict Mode
+'use strict';
+
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true; //Undefined
+if (hasDriversLicense) console.log('I can drive :D');
+
+// Reserves word
+// const interface = 'Audio';
+// const private = 345;
+// const if = 34;
+*/
+
+// Function
+
+function logger() {
+        console.log('My nam is saira');
+}
+
+// Calling / running / invoking function
+logger();
+
+function fruitProcessor(apples, oranges) {
+        console.log(apples, oranges);
+        const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+        return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
