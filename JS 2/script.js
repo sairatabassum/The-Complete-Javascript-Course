77afd3b7a462be1997bcca41a258a7fcc91d2f88
Tabassum <sairatabassum386@gmail.com>
2021-06-20 16:48:18 +0600

    commit

---
+++ JS 2/script.js
@@ -0,0 +1,18 @@
+
+//Activating Strict Mode
+'use strict';
+
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true; //Undefined
+if (hasDriversLicense) console.log('I can drive :D');
+
+// Reserves word
+// const interface = 'Audio';
+// const private = 345;
+// const if = 34;
+*/
+
+// Function
