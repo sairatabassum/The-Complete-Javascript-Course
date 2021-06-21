d8213d0b328ea98486b60c37881ab9f789498e0d
Tabassum <sairatabassum386@gmail.com>
2021-06-21 20:48:38 +0600

    commit

--- 02-Fundamental-Part-2/script.js
+++ 02-Fundamental-Part-2/script.js
@@ -201,84 +201,44 @@ calcAge(years[2])];
 console.log(ages);
 */
 
+/*
 /////////////////////
 // Basic Array Operation
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Add elements
+const friends = ['Michael', 'Steven', 'Peter'];
+const newLength = friends.push('Saira');
+console.log(friends);
+console.log(newLength);
+
+friends.unshift('Asri');
+console.log(friends);
+
+// Remove elements
+friends.pop() // Last
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+friends.shift(); // First
+console.log(friends);
+
+// This return the index at which this element
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob'));
+
+friends.push(23);
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+console.log(friends.includes('23'));
+
+if (friends.includes('Steven')) {
+        console.log('You have a friend called Steven');
+}
+else {
+        console.log('You do not have a friend called Steven');
+}
+*/
+
+/////////////////////
+// Coding Challenge 2
