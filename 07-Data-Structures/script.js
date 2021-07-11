632a7464a846a0bee5280eefc857039d72e6bd9f
Tabassum <sairatabassum386@gmail.com>
2021-07-11 18:44:32 +0600

    commit

--- 07-Data-Structures/script.js
+++ 07-Data-Structures/script.js
@@ -69,3 +69,87 @@ console.log(p, q, r);
 
 ///////////////////////////////////
 // Destructuring Objects
+/*
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0,
+      close: 24,
+    },
+  },
+  order: function (starterIndex, mainIndex, categoriesIndex) {
+    return [
+      this.starterMenu[starterIndex],
+      this.mainMenu[mainIndex],
+      this.categories[categoriesIndex],
+    ];
+  },
+  // Calling a method with object
+
+  // orderDelivery: function (obj) {
+  //   console.log(obj);
+  // },
+
+  // Calling a method with destructuring object
+  orderDelivery: function ({ time, address, mainIndex, starterIndex }) {
+    console.log(`Order received! ${this.starterMenu[starterIndex]}
+and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+  },
+};
+
+// Passing an object into function
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'via del sole,21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+// In object the order of the elements does not matter
+// Exact property name
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+// Variable name, property name : variable name
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+// Trying to read a property that does not exist
+// setting default value
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+// Mutating variable
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 4 };
+
+// Javascript expects a codeblock
+// {a,b}=obj;
+({ a, b } = obj);
+console.log(a, b);
+
+// nested object
+const {
+  fri: { open: op, close: cl },
+} = openingHours;
+console.log(op, cl);
+*/
