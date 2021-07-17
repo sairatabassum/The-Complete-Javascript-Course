e614b429abc8a61efba1fe006c0690dd2c9a27f0
Tabassum <sairatabassum386@gmail.com>
2021-07-17 20:02:00 +0600

    commit

--- 07-Data-Structures/script2.js
+++ 07-Data-Structures/script2.js
@@ -178,7 +178,7 @@ console.log(menu.entries());
 
 /////////////////////////
 // Enhanced Object Literals
-/*
+
 const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 const openingHours = {
   [weekdays[3]]: {
@@ -226,7 +226,7 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
-*/
+
 ////////////////////////////////
 // Optional Chaining (?.) - if a certain property does not exist,
 // then undefined is returned immediately.
@@ -300,6 +300,7 @@ for (const [key, { open, close }] of entries) {
 
 ////////////////////////////////
 // Coding Challenge-2
+/*
 const game = {
   team1: 'Bayern Munich',
   team2: 'Borrussia Dortmund',
@@ -371,10 +372,11 @@ for (const [i, j] of odds) {
 
 // BONUS
 
+*/
 /////////////////////////////////
 // SETS
 // sets just a collection of unique values
-
+/*
 const orderSet = new Set([
   'Pasta',
   'Pizza',
@@ -404,3 +406,70 @@ const staffUnique = [...new Set(staff)];
 console.log(staffUnique);
 
 console.log(new Set('Saira Tabassum').size);
+*/
+
+//////////////////////////////
+// Map
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'we are closed');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get('true'));
+
+const time = 8;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+console.log(rest);
+console.log(rest.size);
+// rest.clear();
+
+const arr = [1, 2];
+rest.set(arr, 'test');
+console.log(rest);
+console.log(rest.get(arr));
+
+rest.set(document.querySelector('h1'), 'heading');
+console.log(rest);
+
+const question = new Map([
+  ['question', 'What is the best programming language'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct'],
+  [false, 'Try again'],
+]);
+
+console.log(question);
+
+// Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+
+console.log(hoursMap);
+
+// Quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`${key}: ${value}`);
+}
+
+const answer = Number(prompt('Your answer:'));
+console.log(answer);
+
+const exam =
+  question.get('correct') === answer ? question.get(true) : question.get(false);
+console.log(exam);
