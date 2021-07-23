3b4b1b8b36687695a9505bc62e2a1335bc85be31
Tabassum <sairatabassum386@gmail.com>
2021-07-23 13:37:13 +0600

    commit

--- 07-Data-Structures/script2.js
+++ 07-Data-Structures/script2.js
@@ -640,3 +640,59 @@ checkBaggage('I have a laptop, some Food and a pocket Knife');
 checkBaggage('Socks and camera');
 checkBaggage('Got some snacks and a gun for protection');
 */
+
+////////////////////////
+// Working With Strings - Part 3
+/*
+// split and join
+console.log('a+very+nice+string'.split('+'));
+console.log('Saira Tabassum'.split(' '));
+
+const [firstName, lastName] = 'Saira Tabassum'.split(' ');
+
+const newName = ['Mrs', firstName, lastName].join(' ');
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+  for (const n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(' '));
+};
+
+capitalizeName('jessica ann smith davis');
+capitalizeName('jonas schmedtmann');
+
+// Padding
+const message = 'Go to gate 23!';
+console.log(message.padStart(25, '+').padEnd(30, '+'));
+console.log('saira'.padStart(25, '+'));
+
+const maskCreditCard = function (number) {
+  const str = number + '';
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+};
+
+console.log(maskCreditCard(237432));
+console.log(maskCreditCard(2873482483266));
+console.log(maskCreditCard('298374824832444444444444456'));
+
+// Repeat
+const message2 = 'Bad weather...All Departures Delayed\n';
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${'🛩'.repeat(n)}`);
+};
+
+planesInLine(2);
+planesInLine(5);
+planesInLine(8);
+*/
+
+////////////////////////////
+// Coding Challenge -4
