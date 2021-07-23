4f0d91439f7770ed7553d77c9eea4aa036bcf6e6
Tabassum <sairatabassum386@gmail.com>
2021-07-24 00:35:56 +0600

    commit

---
+++ 08-Closer-Look-at-Functions/script.js
@@ -0,0 +1,32 @@
+'use strict';
+
+//////////////////////////////
+// Default Parameters
+/*
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  // ES5
+  //   numPassengers = numPassengers || 1;
+  //   price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+createBooking('LJ54');
+createBooking('dsfh', 2);
+createBooking('Ls437', 3, 90);
+
+// so when we don't pass an argument into that parameter, then it will take the value of undefined.
+
+createBooking('LH12', undefined, 1000);
+*/
