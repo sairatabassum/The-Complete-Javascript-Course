31ca515bd2261d2fe119a0e811814cd5f264c7f8
Tabassum <sairatabassum386@gmail.com>
2021-07-28 12:34:27 +0600

    commit

--- 08-Functions/script.js
+++ 08-Functions/script.js
@@ -230,3 +230,68 @@ console.log(addVat2(23));
 
 /////////////////////////////
 // Coding Challenge -1
+/*
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0, 0, 0, 0]. More in the next section 😃
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    // get an answer
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')}\n(write option number)`
+      )
+    );
+    console.log(answer);
+    // Register answer
+    typeof answer === 'number' &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+    this.displayResult();
+    this.displayResult('string');
+  },
+  displayResult(type = 'array') {
+    if (type === 'array') console.log(this.answers);
+    else if (type === 'string')
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+  },
+};
+// poll.registerNewAnswer();
+
+document
+  .querySelector('.btn2')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+// Set the this keyword to a new object
+poll.displayResult.call({ answers: [5, 2, 3] }, 'string');
+poll.displayResult.call({ answers: [1, 2, 3, 4, 4, 5] }, 'string');
+poll.displayResult.call({ answers: [1, 2, 34] });
+*/
+
+//////////////////////////////////
+// Immediately Invoked Function Expressions (IIFE)
+/*
+const runManyTimes = function () {
+  console.log('You can call the function again');
+};
+
+runManyTimes();
+
+// IIFE
+(function () {
+  console.log("You can't call the function again");
+})();
+
+(() => console.log("You can't call the function again"))();
+
+{
+  const isPrivate = 23;
+  var notPrivate = 46;
+}
+console.log(notPrivate);
+// console.log(isPrivate);
+*/
+
+/////////////////////////////////////
+// Closures
