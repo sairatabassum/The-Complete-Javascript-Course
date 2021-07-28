fad2bbc403b373d669e1e9ea4f1d6128a0c5ad24
Tabassum <sairatabassum386@gmail.com>
2021-07-28 22:14:26 +0600

    commit

--- 08-Functions/script.js
+++ 08-Functions/script.js
@@ -295,3 +295,78 @@ console.log(notPrivate);
 
 /////////////////////////////////////
 // Closures
+//  a closure always makes sure that a function does not lose the connection to the variables that were present at its birthplace.
+/*
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+
+booker();
+booker();
+console.dir(booker);
+*/
+
+//////////////////////////
+//  More Closure Examples
+/*
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+// Re-assigning f function
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000;
+boardPassengers(180, 3);
+*/
+
+///////////////////////////////
+// Coding Challenge -2
+/*
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
+*/
