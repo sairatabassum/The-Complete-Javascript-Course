1fe9fe01d3d8179f6b02ac228168334191b240e6
Saira Tabassum <sairatabassum386@gmail.com>
2022-07-04 19:30:02 +0600

    commit

--- 11-Advanced-DOM-Events/bankist.js
+++ 11-Advanced-DOM-Events/bankist.js
@@ -37,12 +37,12 @@ btnScrollTo.addEventListener('click', function (e) {
   // The Element interface's scrollIntoView() method scrolls the element's parent container such that the element on which scrollIntoView() is called is visible to the user.
 });
 
-const h1 = document.querySelector('h1');
-const alertH1 = function (e) {
-  alert('hahahjka');
-  // h1.removeEventListener('mouseenter', alertH1);
-};
-h1.addEventListener('mouseenter', alertH1);
+// const h1 = document.querySelector('h1');
+// const alertH1 = function (e) {
+//   alert('hahahjka');
+//   // h1.removeEventListener('mouseenter', alertH1);
+// };
+// h1.addEventListener('mouseenter', alertH1);
 
 // setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
 
@@ -110,4 +110,67 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
     console.log(id);
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
+});
+
+/////////////////////////////////
+// DOM Traversing
+
+const h1 = document.querySelector('h1');
+
+// Going Downwards: child
+
+// Direct children
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
+
+// Going Upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+// but querySelector, finds children,
+
+// no matter how deep in the Dom tree,
+
+// while the closest method finds parents
+
+// Going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function (el) {
+  if (el !== h1) el.style.transform = 'scale(0.5)';
+});
+
+/////////////////////////
+// Tabbed Component
+const btn_tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  console.log(clicked);
+
+  //   guard clause
+  if (!clicked) return;
+
+  //   Active tab
+  btn_tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  clicked.classList.add('operations__tab--active');
+
+  //   Active tab content
+  tabsContent.forEach(t => t.classList.remove('operations__content--active'));
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
 });
