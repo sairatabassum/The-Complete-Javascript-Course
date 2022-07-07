410e2d4e98f1912a04ec6769b5810ce03c63a032
Saira Tabassum <sairatabassum386@gmail.com>
2022-07-07 10:43:09 +0600

    commit

--- 11-Advanced-DOM-Events/bankist.js
+++ 11-Advanced-DOM-Events/bankist.js
@@ -384,3 +384,17 @@ dotContainer.addEventListener('click', function (e) {
     activateDot(slide);
   }
 });
+
+document.addEventListener('DOMContentLoaded', function (e) {
+  console.log('HTML parsed and DOM tree built', e);
+});
+
+window.addEventListener('load', function (e) {
+  console.log('Page fully loaded', e);
+});
+
+// window.addEventListener('beforeunload', function (e) {
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = '';
+// });
