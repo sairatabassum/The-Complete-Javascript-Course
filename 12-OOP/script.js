9d7d6d2d84e253cdf77a0a2915baaee695126075
Saira Tabassum <sairatabassum386@gmail.com>
2022-07-08 12:33:09 +0600

    commit

--- 12-OOP/script.js
+++ 12-OOP/script.js
@@ -41,3 +41,50 @@ console.log(saira.species);
 
 console.log(saira.hasOwnProperty('firstName'));
 console.log(saira.hasOwnProperty('species'));
+
+console.log(saira.__proto__);
+// Object.prototype (top of prototype chain)
+console.log(saira.__proto__.__proto__);
+console.log(saira.__proto__.__proto__.__proto__);
+
+console.log(Person.prototype.constructor);
+
+const arr = [3, 4, 6, 3, 2, 5, 2, 7, 9];
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+// console.dir(h1);
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed}`);
+};
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed}`);
+};
+
+const BMW = new Car('BMW', 120);
+const Mercedes = new Car('Mercedes', 95);
+
+BMW.accelerate();
+BMW.accelerate();
+BMW.brake();
+BMW.accelerate();
+
+Mercedes.accelerate();
+Mercedes.brake();
