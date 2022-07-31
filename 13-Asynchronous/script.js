fde0ca3c339ba34c22ebc64aac80d32317d06826
sairatabassum <sairatabassum386@gmail.com>
2022-07-31 22:22:55 +0600

    commit

--- 13-Asynchronous/script.js
+++ 13-Asynchronous/script.js
@@ -193,9 +193,54 @@ const whereAmI = function (lat, lng) {
     });
 };
 
-whereAmI(52.508, 13.381);
-whereAmI(19.037, 72.873);
-whereAmI(-33.933, 18.474);
+// whereAmI(52.508, 13.381);
+// whereAmI(19.037, 72.873);
+// whereAmI(-33.933, 18.474);
 
 ////////////////////////////////////////////////
 // The Event Loop in Practice
+// console.log('Test start');
+// setTimeout(() => console.log('0 sec timer'), 0);
+// Promise.resolve('Resolved promise 1').then(res => console.log(res));
+
+// Promise.resolve('Resolved promise 2').then(res => {
+//   for (let i = 0; i < 100; i++) {
+//     console.log(res);
+//   }
+// });
+
+// console.log('Test end');
+
+//////////////////////////////
+// Building a Simple Promise
+const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log('Lottery draw is happenning');
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve('You WIN 🤠');
+    } else {
+      reject(new Error('You lost your money 😥'));
+    }
+  }, 2000);
+});
+
+lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+// Promisifying setTimeout
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+wait(2)
+  .then(res => {
+    console.log('I waited for 2 seconds');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('I waited for 1 second');
+  });
+
+Promise.resolve('saira').then(x => console.log(x));
+Promise.reject(new Error('abc')).catch(x => console.error(x));
